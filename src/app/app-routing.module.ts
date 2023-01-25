3339264f4de5d76bc2732ad621132697c95bf93f
andijayawizard <andijayawizard@gmail.com>
2023-01-25 13:38:45 +0700

    2501

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArticlesComponent } from './articles/articles.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ArticlesComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { enableTracing: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
